b4924a8de3c8e8189868a93158b1342bb3a9260f
Niram <110203162+Niramx0@users.noreply.github.com>
2022-08-03 07:57:22 -0500

    Add files via upload

---
+++ Pokemon/src/co/edu/udistrital/Vista/PanelC2.java
@@ -0,0 +1,24 @@
+package co.edu.udistrital.Vista;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+public class PanelC2 extends JPanel{
+	
+	public PanelC2() {
+		setLayout(new BorderLayout());
+		
+		setVisible(true);
+	}
+
+	public void inicializarComp(){
+		PanelC2_1 pc2_1 = new PanelC2_1();
+		add(pc2_1, BorderLayout.NORTH);
+		
+		PanelC2_2 pc2_2 = new PanelC2_2();
+		add(pc2_2, BorderLayout.CENTER);
+		
+		
+	}
+}
---
+++ Pokemon/src/co/edu/udistrital/mitrabajonovaldranadaXD/main.java
@@ -0,0 +1,13 @@
+package co.edu.udistrital.mitrabajonovaldranadaXD;
+
+import co.edu.udistrital.Vista.PanelPrincipal;
+
+public class main {
+
+	public static void main(String[] args) {
+		PanelPrincipal p = new PanelPrincipal();
+
+	}
+
+
+}
