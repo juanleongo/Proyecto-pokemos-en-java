629806b82a736d8d3d2a612815a44eab33fb59a5
AndresPescador <94087468+AndresPescador@users.noreply.github.com>
2022-08-12 14:36:18 -0500

    Update PanelC2_1.java

--- Pokemon/src/co/edu/udistrital/Vista/PanelC2_1.java
+++ Pokemon/src/co/edu/udistrital/Vista/PanelC2_1.java
@@ -8,7 +8,7 @@ import javax.swing.JPanel;
 /**
  * Clase PanelC2_1
  *
- * Contiene informaciÛn para la creaciÛn del Panel que estara contenido en la parte  izquierda del JPanel PanelC2,que contendra el pokemon elegido por el usuario para combatir.
+ * Contiene informaci√≥n para la creaci√≥n del Panel que estara contenido en la parte  izquierda del JPanel PanelC2,que contendra el pokemon elegido por el usuario para combatir.
  */
 public class PanelC2_1 extends JPanel{
 	/** 
@@ -17,7 +17,7 @@ public class PanelC2_1 extends JPanel{
 	private JLabel Pokemon = new JLabel(); 
 	
 	/** 
-	 * MÈtodo constructor que crea una instancia de la clase PanelC2_1.
+	 * M√©todo constructor que crea una instancia de la clase PanelC2_1.
 	 * <br><b>Pos</b> <br> 
 	 * Se han creado una instancia de la clase PanelC2_1.
 	 * <br>                                           
@@ -30,7 +30,7 @@ public class PanelC2_1 extends JPanel{
 	
 	
 	/** 
-	 * Este mÈtodo inicializa el JLabel "Pokemon" y lo aÒade al JPanel.
+	 * Este m√©todo inicializa el JLabel "Pokemon" y lo a√±ade al JPanel.
 	 * <br><b>Pre</b><br> 
 	 * El layout del JPanel debe ser un GridLayout(1,1)
 	 * <br>
@@ -41,5 +41,31 @@ public class PanelC2_1 extends JPanel{
 	public void inicializarComp() {
 		Pokemon.setText("1");
 		add(Pokemon);
+	}
+	
+	/** 
+	 * Este m√©todo devuelve el label "Pokemon".
+	 * <br><b>Pre</b><br> 
+	 * Debe existir un JLabel con nombre Pokemon.
+	 * <br>
+	 * <br><b>Pos</b><br>  
+	 * Se ha retornado el label correctamente.
+	 * <br>
+	 */
+	public JLabel getPokemon() {
+		return Pokemon;
+	}
+
+	/** 
+	 * Este m√©todo recibe un Jlabel y modifica el label "Pokemon".
+	 * <br><b>Pre</b><br> 
+	 * Debe existir un arreglo de JLabel con nombre Pokemon.
+	 * <br>
+	 * <br><b>Pos</b><br>  
+	 * Se ha modificado el label correctamente.
+	 * <br>
+	 */	
+	public void setPokemon(JLabel pokemon) {
+		Pokemon = pokemon;
 	}
 }
