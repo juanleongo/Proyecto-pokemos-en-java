e748bf9fdff0d4fb76af55fc1486c1ef4a15e8ea
AndresPescador <94087468+AndresPescador@users.noreply.github.com>
2022-08-12 17:50:59 -0500

    Agregada funcionalidad botón modificar

--- src/controlador/Controller.java
+++ src/controlador/Controller.java
@@ -119,6 +119,20 @@ public void inicializar_tipos() {
 			}
 		}
 		
+		if(comando.equals("Modificar")) {
+			nombre= JOptionPane.showInputDialog("Introduzca el nombre del pokemon que desea modificar");
+			boolean existe = existePokemon(nombre);
+			if(existe) {
+				for (int i = 0; i < listaPokemons.size(); i++) {
+					if(listaPokemons.get(i).getNombre().equals(nombre)) {
+						listaPokemons.get(i).setNombre(JOptionPane.showInputDialog("Introduzca el nuevo nombre del pokemon:"));
+						JOptionPane.showMessageDialog(null, "El pokemon ha sido modificado de su equipo.");
+					}
+				}
+			}else {
+				JOptionPane.showMessageDialog(null, "El pokemon "+nombre+" no existe en su equipo.");
+				
+			}
+		}		
 	}
-
 }
